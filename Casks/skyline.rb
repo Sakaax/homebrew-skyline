fa9c030273035206a9453efdf6fb189991edfa63
Sakaax <ducrettetbryan1@gmail.com>
2026-06-30 16:40:40 +0200

    skyline 1.1.1 (build 5) — fix largeur notch Événements

--- Casks/skyline.rb
+++ Casks/skyline.rb
@@ -1,6 +1,6 @@
 cask "skyline" do
-  version "1.1"
-  sha256 "d4fb200fa92f199a19973ec9e4111eb735d7fdca7b260a38c778c6a1e1d5e8e7"
+  version "1.1.1"
+  sha256 "a3bbce49b5e3b48bf4d190e277892815c539993045da59522939caa6e4978a7b"
 
   url "https://skyline.sakaax.com/Skyline.dmg"
   name "Skyline"
